5d91b03d5acf3c69d25467d5a51e113faa18f39d
gmac <gmaccarone@gmail.com>
2019-11-22 14:54:41 -0500

    Add dice.js for basic dice rolling operations

---
+++ dice.js
@@ -0,0 +1,19 @@
+function parse_roll(roll_string) {
+  var regex1 = /(\d+)?d(\d+)/;
+  var results = regex1.exec(roll_string);
+  return {count: results[1], type: results[2]};
+}
+
+function roll_dice(count, type) {
+  var total = 0;
+  var i;
+  for (i=0; i < count; i++) {
+    total += Math.floor(Math.random() * type);
+  }
+  return total;
+}
+
+function roll(roll_string) {
+    var the_dice = parse_roll(roll_string);
+    return roll_dice(the_dice.count, the_dice.type);
+}
